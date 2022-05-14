e6cbe3740f3eafbce6286e6b51ff9d2f80150743
Ivan Bošnjak <ivan.bosnjak@enterwell.net>
2022-05-14 12:00:31 +0200

    FE initial commit with list of activities

---
+++ client-app/src/App.tsx
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import React from 'react';
+import { Header, List } from 'semantic-ui-react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://localhost:5001/api/activities').then(response => {
+      console.log(response);
+      setActivities(response.data);
+    })
+  }, [])
+
+
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Reactivities' />
+      <List>
+          {activities.map((activity: any) => (
+            <List.Item key={activity.id}>
+              {activity.title}
+              </List.Item>
+          ))}
+      </List>
+    </div>
+  );
+}
+
+export default App;
