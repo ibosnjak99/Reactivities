640821f0204674fe2376799746b5b234fdbf8e52
Ivan Bošnjak <ivan.bosnjak@enterwell.net>
2022-08-19 15:05:17 +0200

    Error handling

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,6 +1,8 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
+import { history } from "../..";
 import { Activity } from "../models/Activity";
+import { store } from "../stores/store";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -14,19 +16,34 @@ axios.interceptors.response.use(async response => {
         await sleep(350);
         return response;
 }, (error: AxiosError) => {
-    const {data, status} = error.response!;
+    const {data, status, config}: { data: any; status: number, config: any } = error.response!;
     switch (status) {
         case 400:
-            toast.error('Bad request');
+            if (typeof data === 'string') {
+                toast.error(data);
+            }
+            if (config.method == 'get' && data.errors.hasOwnProperty('id')) {
+                history.push('/not-found');
+            }
+            if (data.errors) {
+                const modalStateErrors = [];
+                for (const key in data.errors) {
+                    if (data.errors[key]) {
+                        modalStateErrors.push(data.errors[key])
+                    }
+                }
+                throw modalStateErrors.flat();
+            } 
             break;
         case 401:
             toast.error('Unauthorised');
             break;
         case 404:
-            toast.error('Not found');
+            history.push('/not-found')
             break;
         case 500:
-            toast.error('Server error');
+            store.commonStore.setServerError(data);
+            history.push('/server-error');
             break;
     }
 
