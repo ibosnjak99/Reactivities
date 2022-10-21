530a5e68a2c96a261e93cfaab57a9cfc534d708d
Ivan Bošnjak <ivan.bosnjak@enterwell.net>
2022-10-21 15:22:11 +0200

    Client side login and registration

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,10 +1,27 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
-import { Container } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { Button, Container, Header } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import LoginForm from "../users/LoginForm";
+import RegisterForm from "../users/RegisterForm";
 
-export default function HomePage() {
+export default observer (function HomePage() {
+    const {userStore, modalStore} = useStore();
     return (
         <Container style={{marginTop: '7em'}}>
-            <h1>HomePage</h1>
+            <h1>Homepage</h1>
+            {userStore.isLoggedIn ? (
+                <>
+                    <h2>Welcome</h2>
+                    <Button as={Link} to='/activities' size='huge' inverted > Activities </Button>
+                </>
+            ) : (
+                <>
+                    <Button onClick={() => modalStore.openModal(<LoginForm />)} size='huge' inverted > Login </Button>
+                    <Button onClick={() => modalStore.openModal(<RegisterForm />)} size='huge' inverted > Register </Button>
+                </>
+            )}
         </Container>
     )
-}
+})
